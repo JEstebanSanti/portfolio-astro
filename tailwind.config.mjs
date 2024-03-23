93f0adbbcba71e9e25609bafcb853d92648e3a5d
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2024-03-22 17:47:01 -0700

    feature: section title, tailwing colors extended, readme update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'cbg-dark': '#232323',
+				'cbg-white': '#FFFDF7',
+				'c-light-blue': '#D0EAF5',
+				'c-light-purple': '#736CED'
+			}
+		},
 	},
 	plugins: [],
 }
